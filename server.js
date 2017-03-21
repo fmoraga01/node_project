90ed9de2bd85424e0423005fedd616d304b2c862
fmoraga01 <fmoraga01@gmail.com>
2017-03-21 17:49:01 -0300

    nuevos cambios

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var express = require('express');
 
-var app = express();
+var express = require(‘express’); 
+var app = express.createServer(); 
+var port = process.env.PORT || 3000;
+
+
+
+//var express = require('express');
+
+//var app = express();
 
 app.set('view engine', 'pug');
 
@@ -18,8 +25,10 @@ app.get('/historia', function(req, res){
 	})
 });
 
-app.listen(3000, function(err){
+/*app.listen(3000, function(err){
 	if(err) return console.log('hubo un error'), process.exit(1);
 
 	console.log('App de prueba escuchando en el puerto 3000');
-});
+});*/
+
+app.listen(port);
